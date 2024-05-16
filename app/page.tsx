ff0cdd83558b615c22c3977629ce407273a4df67
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-05-16 17:45:04 +0530

    added auth and database

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function Home() {
+  
   return (
     <div>
       <Header/>
